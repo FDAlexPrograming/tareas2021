4b3fbf60904d2308993094ccc5f0405843c726c5
Alex Farias Dos Santos <fariasdossantosalex@gmail.com>
2021-09-17 13:19:46 -0300

    otro

--- basededatos.php
+++ basededatos.php
@@ -2,5 +2,11 @@
 
 function getTareas(){
     $basededatos = new PDO('mysql:host=localhost;'.'dbname=basededatos_tareas;charse=utf8','root','');
-    $sentencia = $basededatos->prepare("select * from tarea");
+    $sentencia = $basededatos->prepare("select * from tareas");
+    $sentencia->execute();
+    $tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    foreach($tareas as $tarea){
+        echo $tarea->titulo;
+    }
+   
 }
